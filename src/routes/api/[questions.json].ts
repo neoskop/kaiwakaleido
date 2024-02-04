4c2fc18fa1380c2db575b46259782d604af851d7
nbank <bank@neoskop.de>
2024-02-04 17:34:28 +0100

    feat: add more data

--- src/routes/api/[questions.json].ts
+++ src/routes/api/[questions.json].ts
@@ -4,7 +4,13 @@ import database from './database.json';
 export default async function Question({ reply }: { reply: FastifyReply }) {
   reply.header('Content-Type', 'application/json');
 
+  const questionsPerCategory = database.reduce((acc, data) => {
+    acc[data.category] = (acc[data.category] || 0) + 1;
+    return acc;
+  }, {});
+
   return JSON.stringify({
-    questions: database.length,
+    all: database.length,
+    ...questionsPerCategory,
   });
 }
