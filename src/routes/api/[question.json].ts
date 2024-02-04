3d317fbb8e3cf1009899bd460813b5b30910d831
nbank <bank@neoskop.de>
2024-02-04 09:07:49 +0100

    fix: remove font & remove directus as database

--- src/routes/api/[question.json].ts
+++ src/routes/api/[question.json].ts
@@ -1,11 +1,5 @@
 import { FastifyReply } from 'fastify';
-import data from './questions.json';
-
-const count = new URL(
-  '/items/kaiwakaleido',
-  String(process.env.DIRECTUS_BASE_URL)
-);
-count.searchParams.append('aggregate[count]', '*');
+import database from './database.json';
 
 type Categorys =
   | 'career_and_motivation'
@@ -22,38 +16,14 @@ const CATEGORY_NAMES: Record<Categorys, string> = {
   personal_development: 'Persönliche Entwicklung',
   future_plans: 'Zukunftspläne',
   personal_values_and_beliefs: 'Persönliche Werte und Übungen',
-  interests_and_hobbies: 'Interessen und Leidenschaften',
+  interests_and_passions: 'Interessen und Leidenschaften',
   memories_and_stories: 'Erinnerungen und Geschichten',
 };
 
 export default async function Question({ reply }: { reply: FastifyReply }) {
   reply.header('Content-Type', 'application/json');
 
-  const question = new URL(
-    '/items/kaiwakaleido',
-    String(process.env.DIRECTUS_BASE_URL)
-  );
-
-  const countResponse = await fetch(count);
-
-  const allItems = (await countResponse.json()).data[0].count;
-
-  console.log(question.searchParams);
-
-  question.searchParams.append('limit', '1');
-  question.searchParams.append(
-    'page',
-    `${Math.floor(Math.random() * Number(allItems))}`
-  );
-  question.searchParams.append('fields', 'category,question');
-
-  const questionResponse = await fetch(question);
-
-  const {
-    data: [data],
-  } = (await questionResponse.json()) as {
-    data: { category: Categorys; question: string }[];
-  };
+  const data = database[Math.floor(Math.random() * database.length)];
 
   return JSON.stringify({
     question: data.question,
